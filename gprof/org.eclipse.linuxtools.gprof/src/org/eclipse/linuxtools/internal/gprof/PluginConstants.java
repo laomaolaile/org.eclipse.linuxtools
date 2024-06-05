d53ab4cdeee126833be58ad4b369cd99898077f8
zhangzegang <116530487@qq.com>
2024-06-05 17:38:41 +0800

    add gprof call graph

--- gprof/org.eclipse.linuxtools.gprof/src/org/eclipse/linuxtools/internal/gprof/PluginConstants.java
+++ gprof/org.eclipse.linuxtools.gprof/src/org/eclipse/linuxtools/internal/gprof/PluginConstants.java
@@ -1,5 +1,6 @@
 package org.eclipse.linuxtools.internal.gprof;
 
+import java.io.File;
 import java.net.URI;
 
 import org.eclipse.core.resources.IProject;
@@ -12,16 +13,13 @@ public final class PluginConstants {
 	public static String getDefaultIOPath(IProject project) {
 		if (project != null) {
 			URI locationURI = project.getLocationURI();
+			File file = new File(locationURI);
 			if (locationURI != null) {
-				STAP_GRAPH_DEFAULT_IO_PATH = locationURI.getPath() + "/callgraph.out"; //$NON-NLS-1$
+				STAP_GRAPH_DEFAULT_IO_PATH = file + "/callgraph.out"; //$NON-NLS-1$
 			}
 		} else {
 			STAP_GRAPH_DEFAULT_IO_PATH = CallgraphCorePlugin.getDefault().getStateLocation().toString()
 					+ "/callgraph.out"; //$NON-NLS-1$
-		}
-
-		if (STAP_GRAPH_DEFAULT_IO_PATH.startsWith("/")) {
-			STAP_GRAPH_DEFAULT_IO_PATH = STAP_GRAPH_DEFAULT_IO_PATH.substring(1); // 删除第一个字符（即前导斜杠）
 		}
 
 		return STAP_GRAPH_DEFAULT_IO_PATH;
